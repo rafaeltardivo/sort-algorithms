0e91eda89ef2ee2f524f7e6e904600803e2022a6
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-20 15:24:42 -0300

    Add second bubble sort implementation

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,26 +7,57 @@ def first_bubble_sort(numbers):
     Args:
         numbers (list): list of integers to be sorted.
     Returns:
-        iterations (int): number of iterations the algorithm took to order 
+        iterations (int): number of iterations the algorithm took to order
         the list.
     """
     size = len(numbers)
     iterations = 0
 
-    for i in range(size -1):
+    for i in range(size - 1):
         iterations += 1
-        for j in range(size -1):
+        for j in range(size - 1):
             iterations += 1
             if numbers[j] > numbers[j + 1]:
                 numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
     return iterations
 
 
-if __name__ == '__main__':
-    first_numbers = [7, 3, 6, 2, 0]
+def second_bubble_sort(numbers):
+    """Second implementation of bubble sort. Contains the first optimization:
+    excluding the last sorted item at each iteration. The iteration counter
+    will be present to show the difference between each implementation.
+
+    Args:
+        numbers (list): list of integers to be sorted.
+    Returns:
+        iterations (int): number of iterations the algorithm took to order
+        the list.
+    """
+    size = len(numbers)
+    iterations = 0
+
+    for i in range(size - 1):
+        iterations += 1
+        for j in range(size - i - 1):
+            iterations += 1
+            if numbers[j] > numbers[j + 1]:
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
+    return iterations
+
+
+if __name__ == "__main__":
+    first_numbers = [6, 3, 7, 8, 9]
 
     iterations = first_bubble_sort(first_numbers)
     print(
-        f'first_bubble_sort result: {first_numbers}.'
-        f' Sorted after {iterations} iterations.'
+        f"first_bubble_sort result: {first_numbers}."
+        f" Sorted after {iterations} iterations."
+    )
+
+    second_numbers = [6, 3, 7, 8, 9]
+
+    iterations = second_bubble_sort(second_numbers)
+    print(
+        f"second_bubble_sort result: {second_numbers}."
+        f" Sorted after {iterations} iterations."
     )
