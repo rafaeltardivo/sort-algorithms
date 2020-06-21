d89547d579947bead8cd834d33fa037588b81f54
Rafael Tardivo <rafaeldt.br@gmail.com>
2020-06-20 21:23:04 -0300

    Add first insertion sort implementation

---
+++ insertion_sort.py
@@ -0,0 +1,38 @@
+def first_insertion_sort(numbers):
+    """Non recursive first implementation of insertion sort.This one does
+    not have any optimization and it is considered to be the worst one. The
+    iteration counter will be present to show the difference between each
+    implementation.
+
+    Args:
+        numbers (list): list of integers to be sorted.
+    Returns:
+        numbers (list): sorted list.
+        iterations (int): number of iterations the algorithm took to sort
+        the list.
+
+    Args:
+        numbers (list): list of integers to be sorted.
+    Returns:
+        numbers (list): sorted list.
+    """
+    size = len(numbers)
+    iterations = 0
+
+    for i in range(size):
+        iterations += 1
+        j = i
+        while j > 0:
+            iterations += 1
+            if numbers[j] < numbers[j - 1]:
+                numbers[j], numbers[j - 1] = numbers[j - 1], numbers[j]
+            j -= 1
+    return numbers, iterations
+
+
+if __name__ == "__main__":
+    numbers, iterations = first_insertion_sort([6, 3, 7, 8, 9])
+    print(
+        f"first_insertion_sort result:  {numbers}."
+        f" Sorted after {iterations} iterations."
+    )
